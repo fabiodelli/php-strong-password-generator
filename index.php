4cd0e33e62f217bffba57c336c421a38ff75d3ba
fabiodelli <ntqdel@hotmail.com>
2023-05-05 17:09:43 +0200

    add function

--- index.php
+++ index.php
@@ -15,7 +15,23 @@
                 <input type="number" id="length" name="length" min="1" max="100">
                 <button type="submit">Generate</button>
             </form>
+            <?php
+		if (isset($_GET['length'])) {
+			$password = generatePassword($_GET['length']);
+			echo "<p>Your password is: $password</p>";
+		}
+		function generatePassword($length) {
+			$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
+			$randomString = '';
+			for ($i = 0; $i < $length; $i++) {
+				$randomString .= $characters[rand(0, strlen($characters) - 1)];
+			}
+			return $randomString;
+		}
+	?>
     </div>
+    
+    
 <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 <script src="./assets/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
